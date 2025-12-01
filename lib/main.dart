80e1b2c470e4c7960c54bf49d9bf1e7662104765
Dirk <so0mewhat2@gmail.com>
2025-12-01 10:59:58 +0100

    scaffold widget

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,6 @@ void main() {
   runApp(const MyApp());
 }
 
-String name = "test";
-int number = 1;
-double test = 1.0;
-List myList = ["test", "test2"];
-Map<String, dynamic> myMap = {'String': "String", "String2": 44444};
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -17,64 +11,20 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
+      theme: ThemeData.from(
         colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.deepPurple,
+          seedColor: Colors.teal,
           brightness: Brightness.dark,
         ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Wrap(
-        children: [
-          Text("anything"),
-          Text("anything"),
-          Text("anything"),
-          Text("anything"),
-          Text("anything"),
-          Text("anything"),
-          Text("anything"),
-          Text("anything"),
-        ],
-      ),
-
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Flutter"),
+          centerTitle: true,
+          leading: Icon(Icons.login),
+          actions: [Text("Action"), Icon(Icons.login)],
+          backgroundColor: Colors.teal,
+        ),
       ),
     );
   }
