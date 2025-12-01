4949dc156a6d3346892db384082c086256a90725
Dirk <so0mewhat2@gmail.com>
2025-12-01 14:37:28 +0100

    create WidgetTree, profile page & home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/views/widget_tree.dart';
 import 'package:flutter_application_1/widgets/navbar_widget.dart';
 
 void main() {
@@ -20,24 +21,7 @@ class MyApp extends StatelessWidget {
           brightness: Brightness.dark,
         ),
       ),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePage();
-}
-
-class _MyHomePage extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text(title!), centerTitle: true),
-      bottomNavigationBar: NavbarWidget(),
+      home: WidgetTree(),
     );
   }
 }
---
+++ lib/views/widget_tree.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/views/pages/home_page.dart';
+import 'package:flutter_application_1/views/pages/profile_page.dart';
+import 'package:flutter_application_1/widgets/navbar_widget.dart';
+
+List<Widget> pages = [HomePage(), ProfilePage()];
+
+class WidgetTree extends StatelessWidget {
+  const WidgetTree({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Flutter"), centerTitle: true),
+      body: pages.elementAt(0),
+      bottomNavigationBar: NavbarWidget(),
+    );
+  }
+}
