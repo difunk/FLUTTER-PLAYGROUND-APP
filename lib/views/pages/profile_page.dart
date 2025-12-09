ab6f2ed811f8812ae733ab28bd8380084eecb557
Dirk <so0mewhat2@gmail.com>
2025-12-09 08:52:08 +0100

    singleChildScrollView, switchListTile, slider, inkWell, elevatedButton

--- lib/views/pages/profile_page.dart
+++ lib/views/pages/profile_page.dart
@@ -11,42 +11,78 @@ class _ProfilePageState extends State<ProfilePage> {
   TextEditingController controller = TextEditingController();
   bool? isChecked = false;
   bool? isApproved = false;
+  bool isSwitched = false;
+  double sliderValue = 0.0;
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(20.0),
-      child: Column(
-        children: [
-          TextField(
-            controller: controller,
-            decoration: InputDecoration(border: OutlineInputBorder()),
-            onEditingComplete: () {
-              setState(() {});
-            },
-          ),
-          Text(controller.text),
-          CheckboxListTile(
-            tristate: true,
-            title: Text("Check"),
-            value: isChecked,
-            onChanged: (bool? value) {
-              setState(() {
-                isChecked = value;
-              });
-            },
-          ),
-          CheckboxListTile(
-            tristate: true,
-            title: Text("Approve"),
-            value: isApproved,
-            onChanged: (bool? value) {
-              setState(() {
-                isApproved = value;
-              });
-            },
-          ),
-        ],
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            TextField(
+              controller: controller,
+              decoration: InputDecoration(border: OutlineInputBorder()),
+              onEditingComplete: () {
+                setState(() {});
+              },
+            ),
+            Text(controller.text),
+            CheckboxListTile.adaptive(
+              tristate: true,
+              title: Text("Check"),
+              value: isChecked,
+              onChanged: (bool? value) {
+                setState(() {
+                  isChecked = value;
+                });
+              },
+            ),
+            CheckboxListTile.adaptive(
+              tristate: true,
+              title: Text("Approve"),
+              value: isApproved,
+              onChanged: (bool? value) {
+                setState(() {
+                  isApproved = value;
+                });
+              },
+            ),
+            SwitchListTile.adaptive(
+              title: Text("Switch"),
+              value: isSwitched,
+              onChanged: (bool value) {
+                setState(() {
+                  isSwitched = value;
+                });
+              },
+            ),
+            Slider.adaptive(
+              max: 10.0,
+              divisions: 10,
+              value: sliderValue,
+              onChanged: (double value) {
+                setState(() {
+                  sliderValue = value;
+                });
+              },
+            ),
+            InkWell(
+              onTap: () {
+                print("image selected");
+              },
+              child: Container(
+                height: 50,
+                width: double.infinity,
+                color: Colors.white12,
+              ),
+            ),
+            SizedBox(height: 10),
+            ElevatedButton(onPressed: () {}, child: Text("Click me")),
+          ],
+        ),
       ),
     );
   }
