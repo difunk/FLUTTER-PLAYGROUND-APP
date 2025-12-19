0b972e873421cec3d6cbd5a87a8b536ffdce6d9a
Dirk <so0mewhat2@gmail.com>
2025-12-19 13:47:00 +0100

    container widget, ...list, column list

--- lib/views/pages/home_page.dart
+++ lib/views/pages/home_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/data/constants.dart';
+import 'package:flutter_application_1/views/widgets/container_widget.dart';
 import 'package:flutter_application_1/views/widgets/hero_widget.dart';
 
 class HomePage extends StatelessWidget {
@@ -8,29 +8,24 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.all(20.0),
+      padding: EdgeInsets.symmetric(horizontal: 20.0),
       child: SingleChildScrollView(
         child: Column(
           children: [
             HeroWidget(title: "Home"),
-            Container(
-              width: double.infinity,
-              padding: EdgeInsets.symmetric(vertical: 10.0),
-              child: Card(
-                child: Padding(
-                  padding: EdgeInsets.all(20.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text("basic layout", style: KTextStyle.titleText),
-                      Text(
-                        "basic description",
-                        style: KTextStyle.descriptionText,
-                      ),
-                    ],
-                  ),
-                ),
-              ),
+            ...List.generate(2, (index) {
+              return ContainerWidget(
+                title: "Title",
+                description: "Description",
+              );
+            }),
+            Column(
+              children: List.generate(2, (index) {
+                return ContainerWidget(
+                  title: "Title",
+                  description: "Description",
+                );
+              }),
             ),
           ],
         ),
