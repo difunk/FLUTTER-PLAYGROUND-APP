a3017aa6c8c9fe6ebe758f7d4d74f4d0bead4fee
Dirk <so0mewhat2@gmail.com>
2025-12-24 16:10:41 +0100

    fetch data via api request

--- lib/views/pages/course_page.dart
+++ lib/views/pages/course_page.dart
@@ -1,10 +1,37 @@
+import 'dart:nativewrappers/_internal/vm/lib/math_patch.dart';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/views/widgets/hero_widget.dart';
+import 'dart:convert' as convert;
+import 'package:http/http.dart' as http;
 
-class CoursePage extends StatelessWidget {
+class CoursePage extends StatefulWidget {
   const CoursePage({super.key});
 
   @override
+  State<CoursePage> createState() => _CoursePageState();
+}
+
+class _CoursePageState extends State<CoursePage> {
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
+  void getData() async {
+    var url = Uri.https('bored-api.appbrewery.com', '/random');
+    var response = await http.get(url);
+    if (response.statusCode == 200) {
+      var jsonResponse =
+          convert.jsonDecode(response.body) as Map<String, dynamic>;
+      var activity = jsonResponse['activity'];
+      print(activity);
+    } else {
+      print('Request failed with status: ${response.statusCode}.');
+    }
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
